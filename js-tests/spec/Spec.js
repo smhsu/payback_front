742ed19c2462cd08a1e4c5077aebc4220b98f8d6
Silas Hsu <hsu.silas@wustl.edu>
2016-04-25 11:18:33 -0500

    Fixed the unit tests

--- js-tests/spec/Spec.js
+++ js-tests/spec/Spec.js
@@ -1,14 +1,14 @@
 describe("Modifiy table", function() {
 	it ("Should be able to correctly populate the expense table for friend 2", function() {
 		var tbody = constructExpenseTableBody(2)[1];
-		expect(tbody.children().length).toBe(2);
+		expect(tbody.children().length).toBe(4);
 		expect(tbody.find('.success').text()).toEqual("They owe $15");
 		expect(tbody.find('.danger').text()).toEqual("You owe $200");
 	});
 
 	it ("Should be able to correctly populate the expense table for friend 3", function() {
 		var tbody = constructExpenseTableBody(3)[1];
-		expect(tbody.children().length).toBe(1);
+		expect(tbody.children().length).toBe(2);
 		expect(tbody.find('.success').text()).toEqual("They owe $15");
 	});
 });
